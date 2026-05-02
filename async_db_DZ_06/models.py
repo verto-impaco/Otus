ddec39898aa4f717d3e3f4305fb2acc9d75a7562
verto-impaco <89851602283v@gmail.com>
2026-05-02 21:43:15 +0300

    Update and rename name.txt to models.py

---
+++ async_db_DZ_06/models.py
@@ -0,0 +1,51 @@
+"""
+создайте алхимичный engine
+добавьте declarative base (свяжите с engine)
+создайте объект Session
+добавьте модели User и Post, объявите поля:
+для модели User обязательными являются name, username, email
+для модели Post обязательными являются user_id, title, body
+создайте связи relationship между моделями: User.posts и Post.user
+"""
+
+import os
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+PG_CONN_URI = os.environ.get(
+    "SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+
+
+async_engine = create_async_engine(PG_CONN_URI)
+
+
+AsyncSessionLocal = async_sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False,)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    username = Column(String(50), unique=True)
+    email = Column(String(100), unique=True)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    userId = Column('user_id', Integer, ForeignKey(
+        'users.id'), nullable=False, index=True)
+    title = Column(String(255), nullable=False)
+    body = Column(Text, nullable=False)
