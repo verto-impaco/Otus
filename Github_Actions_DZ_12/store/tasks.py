c1ceed5d84c44149f1be4d00ad7e99c333236d53
verto-impaco <89851602283v@gmail.com>
2026-07-03 14:19:15 +0300

    Delete Github_Actions_DZ_12/store directory

--- Github_Actions_DZ_12/store/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-
-
-@shared_task
-def log_product_created(product_id, product_name):
-    message = f"Новый товар добавлен: {product_name} (ID: {product_id})"
-    print(message, flush=True)
-    return message
